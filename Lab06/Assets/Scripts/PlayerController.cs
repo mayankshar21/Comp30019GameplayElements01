c3283b3b99a0d317dec0f6115f908061315cb5f8
Mayank Sharma <mayankshar21@yahoo.com.sg>
2018-09-04 09:51:47 +1000

    --Code Updated
    --Code Updated from lab06

---
+++ Lab06/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 1.0f; // Default speed sensitivity
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Lab06/Assets/Scripts/SwarmManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwarmManager : MonoBehaviour {
+
+    // External parameters/variables
+    public GameObject enemyTemplate;
+    public int enemyRows;
+    public int enemyCols;
+    public float enemySpacing;
+    public float stepSize;
+    public float stepTime;
+    public float maxXDeviation; // Largest/smallest X-coordinate the edge of the swarm should reach
+
+    // Internal parameters/variables
+    private float stepCountdown;
+
+    // Use this for initialization
+    void Start () {
+        GenerateSwarm();
+
+        // Initial parameters
+        this.stepCountdown = this.stepTime;
+        this.transform.localPosition = Vector3.left * maxXDeviation; // Start at far left
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.stepCountdown -= Time.deltaTime;
+        if (this.stepCountdown < 0.0f)
+        {
+            // Perform a single step to move the swarm across (or down)
+            // Then reset the timer to periodically perform such steps
+            this.StepSwarm();
+            this.stepCountdown = this.stepTime;
+        }
+    }
+
+    // Method to automatically generate swarm of enemies based on the set public attributes
+    private void GenerateSwarm()
+    {
+        // Task 4b ...
+    }
+
+    // Method to step a swarm across the screen (or down & reverse when it reaches the edge)
+    private void StepSwarm()
+    {
+        // Task 4c ...
+    }
+}
